35471628777f5b426b24517d7fe33c1290eba361
wayne-otido <otidowayne@gmail.com>
2023-08-28 15:36:37 +0300

    initial

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears 
+    (id , name, age, sex, color, temperature, alive)
+VALUES
+    (1, 'Mr. Chocolate', 7, 'M', 'brown', 'warm', True),
+    (2, 'Rowdy', 8, 'M', 'white', 'cold', True),
+    (3, 'Tabitha' , 9, 'F', 'black', 'hot', True),
+    (4, 'Sergeant Brown', 10, 'M', 'brown', 'warm', False),
+    (5, 'Melissa' , 11, 'F', 'white', 'cold', False),
+    (6, 'Grinch', 9, 'M', 'black', 'hot', True),
+    (7, 'Wendy', 7, 'F', 'brown', 'warm', True),
+    (8, NULL, 10, 'F', 'white', 'cold', False)
